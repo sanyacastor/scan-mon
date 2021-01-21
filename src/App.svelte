8cbcdcf47e8874e53c7a3ee332a6ecdea3caeac5
castor <donbobrino@gmail.com>
2021-01-21 23:43:41 +0300

    filter for uniq IDs

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,90 @@
 <script>
-	import Map from "./Map.svelte";
-	import List from "./List.svelte";
-	import Header from "./Header.svelte";
-	import MapMarker from "./MapMarker.svelte";
+  import Map from "./Map.svelte";
+  import List from "./List.svelte";
+  import Header from "./Header.svelte";
+  import MapMarker from "./MapMarker.svelte";
 
-	const fetchItems = (async () => {
-		const response = await fetch(
-			"https://sheets.googleapis.com/v4/spreadsheets/1aAgF1OvlDSBYhkw5keCutXeMM6T2Vzpw0P1uYdIIeJE/values/Sheet1!A1:E1000?key=AIzaSyC-UMHJ9ffwcgsA5u3Qm8xd5pcXPMYU4Wo"
-		);
-		const data = await response.json();
-		return await data.values.slice(1);
-	})();
+  const fetchItems = (async () => {
+    const response = await fetch(
+      "https://sheets.googleapis.com/v4/spreadsheets/1aAgF1OvlDSBYhkw5keCutXeMM6T2Vzpw0P1uYdIIeJE/values/Sheet1!A1:E1000?key=AIzaSyC-UMHJ9ffwcgsA5u3Qm8xd5pcXPMYU4Wo"
+    );
+    const data = await response.json();
+
+    let uniqItems = [];
+    let uniqIds = new Set();
+    let items = data.values.slice(1);
+    items.forEach((item) => uniqIds.add(item[2]));
+
+    const getDate = (str) =>
+      `${str.split(" ")[0].split("/")[2]}-${str.split(" ")[0].split("/")[1]}-
+     ${str.split(" ")[0].split("/")[0]}T${str.split(" ")[1]}:00`;
+
+    uniqIds.forEach((u) => {
+      const sameId = items.filter((item) => item[2] == u);
+      let last = sameId[0];
+      for (let i = 0; i < sameId.length; i++) {
+        const current = sameId[i];
+        if (getDate(current[1]) > getDate(last[1])) {
+          last = current;
+        }
+      }
+      uniqItems.push(last);
+    });
+    return uniqItems;
+  })();
 </script>
 
+<main>
+  <section class="container">
+    {#await fetchItems}
+      <div class="loader"><span>loading...</span></div>
+    {:then data}
+      <Header />
+      <List class="list" items={data} />
+      <Map lat={40.4165} lon={-3.70256} zoom={5}>
+        {#each data as item}
+          <MapMarker lat={item[3]} lon={item[4]} label={item[2]} />
+        {/each}
+      </Map>
+    {:catch error}
+      <p>An error occurred!</p>
+    {/await}
+  </section>
+</main>
+
 <style>
-	main {
-		padding: 0;
-		width: 100%;
-		height: 100vh;
-		box-sizing: border-box;
-	}
+  main {
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    box-sizing: border-box;
+  }
 
-	.container {
-		display: grid;
-		width: 100%;
-		max-height: 100vh;
-		grid-template-columns: 1fr 1fr;
-		grid-template-rows: 40px 1fr;
-		/* overflow: hidden; */
-	}
+  .container {
+    display: grid;
+    width: 100%;
+    max-height: 100vh;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 40px 1fr;
+    /* overflow: hidden; */
+  }
 
-	.loader {
-		width: 100%;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  .loader {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-	/* .list {
+  /* .list {
 		grid-column-start: 0;
 		grid-column-end: 1;
 	} */
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
-
-<main>
-	<section class="container">
-		{#await fetchItems}
-			<div class="loader"><span>loading...</span></div>
-		{:then data}
-			<Header />
-			<List class="list" items={data} />
-			<Map lat={data[0][3]} lon={data[0][4]} zoom={15}>
-				{#each data as item}
-					<MapMarker lat={item[3]} lon={item[4]} label={item[2]} />
-				{/each}
-			</Map>
-		{:catch error}
-			<p>An error occurred!</p>
-		{/await}
-	</section>
-</main>
--- src/List.svelte
+++ src/List.svelte
@@ -1,76 +1,86 @@
 <script>
-    export let items;
-    export let search = "";
-    export let filtredItems = items;
+  export let items;
+  export let search = "";
+  export let filtredItems = items;
 </script>
 
-<style>
-    .items {
-        box-sizing: border-box;
-        height: calc(100vh - 40px);
-    }
-    ul {
-        list-style: none;
-        width: 100%;
-        padding: 0;
-        margin: 0;
-        max-height: 400px;
-        overflow: auto;
-    }
-    li {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 16px 8px;
-        display: flex;
-        justify-content: space-between;
-        border-top: 1px solid rgb(212, 212, 212);
-        cursor: pointer;
-        transition: all 300ms ease;
-    }
-
-    li:last-of-type {
-        border-bottom: 1px solid rgb(212, 212, 212);
-    }
-    li:hover {
-        background: rgb(98, 30, 153);
-        color: #fff;
-    }
-    .search {
-        width: 100%;
-    }
-    .search-wrapper {
-        box-sizing: border-box;
-        padding: 16px;
-        border-bottom: 1px solid rgb(212, 212, 212);
-    }
-    h2 {
-        padding-left: 8px;
-        text-decoration: underline;
-        text-transform: uppercase;
-    }
-</style>
-
 <div class="items">
-    <label class="search-wrapper">
-        <input
-            type="text"
-            class="search"
-            placeholder="search"
-            on:input={(e) => (filtredItems = items.filter((i) =>
-                    i[2]
-                        .toLowerCase()
-                        .includes(e.target.value.trim().toLowerCase())
-                ))}
-            bind:value={search} />
-    </label>
-    <h2>Tracking</h2>
-    <ul>
-        {#each filtredItems as item}
-            <li data-id={item[2]}>
-                <span>🤖 {item[2]}</span>
-                <span style="padding-right: 8px">📆
-                    {item[1].split('/').join('.').split(' ')[0]}</span>
-            </li>
-        {/each}
-    </ul>
+  <label class="search-wrapper">
+    <input
+      type="text"
+      class="search"
+      placeholder="search"
+      on:input={(e) =>
+        (filtredItems = items.filter((i) =>
+          i[2].toLowerCase().includes(e.target.value.trim().toLowerCase())
+        ))}
+      bind:value={search}
+    />
+  </label>
+  <h2>Tracking list</h2>
+  <ul>
+    {#each filtredItems as item, i}
+      <li data-id={item[2]}>
+        <span>{i}.🤖 {item[2]}</span>
+        <span style="padding-right: 8px"
+          >📆
+          {new Date(
+            `${item[1].split(" ")[0].split("/")[2]}-${
+              item[1].split(" ")[0].split("/")[1]
+            }-${item[1].split(" ")[0].split("/")[0]}T${
+              item[1].split(" ")[1]
+            }:00`
+          ).toLocaleString()}</span
+        >
+      </li>
+    {/each}
+  </ul>
 </div>
+
+<style>
+  .items {
+    box-sizing: border-box;
+    height: calc(100vh - 40px);
+    display: grid;
+    grid-template-rows: auto auto 1fr;
+  }
+  ul {
+    list-style: none;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    overflow: auto;
+    grid-row-start: 3/4;
+  }
+  li {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 16px 8px;
+    display: flex;
+    justify-content: space-between;
+    border-top: 1px solid rgb(212, 212, 212);
+    cursor: pointer;
+    transition: all 300ms ease;
+  }
+  li:last-of-type {
+    border-bottom: 1px solid rgb(212, 212, 212);
+  }
+  li:hover {
+    background: rgb(98, 30, 153);
+    color: #fff;
+  }
+  .search {
+    width: 100%;
+  }
+  .search-wrapper {
+    box-sizing: border-box;
+    padding: 16px;
+    border-bottom: 1px solid rgb(212, 212, 212);
+  }
+  h2 {
+    padding-left: 8px;
+    text-decoration: underline;
+    text-transform: uppercase;
+    text-align: center;
+  }
+</style>
