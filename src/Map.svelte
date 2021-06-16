e9feaea74cc2c4d92baede2549ed70e343effd02
sanyacastor <donbobrino@gmail.com>
2021-06-16 10:19:05 +0300

    map controls update

--- src/Map.svelte
+++ src/Map.svelte
@@ -12,12 +12,12 @@
 
   let container;
   let map;
+  let mapStyle = "mapbox://styles/mapbox/satellite-v9";
 
   onMount(() => {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = "https://unpkg.com/mapbox-gl/dist/mapbox-gl.css";
-
     link.onload = () => {
       map = new mapbox.Map({
         container,
@@ -25,8 +25,29 @@
         center: [lon, lat],
         zoom,
       });
+
+      map.addControl(
+        new mapbox.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+        })
+      );
     };
+
     document.head.appendChild(link);
+    document
+      .getElementById("switch-style-button")
+      .addEventListener("click", switchLayer);
+
+    function switchLayer() {
+      mapStyle =
+        mapStyle === "mapbox://styles/mapbox/satellite-v9"
+          ? "mapbox://styles/mapbox/streets-v9"
+          : "mapbox://styles/mapbox/satellite-v9";
+      map.setStyle(mapStyle);
+    }
 
     return () => {
       map.remove();
@@ -36,12 +57,25 @@
 </script>
 
 <div bind:this={container}>
+  <button id="switch-style-button">Спутник</button>
   {#if map}
     <slot />
   {/if}
 </div>
 
 <style>
+  button {
+    position: absolute;
+    z-index: 99;
+    /* width: 40px; */
+    height: 40px;
+    border-radius: 50%;
+    top: 16px;
+    left: 16px;
+    background: rgb(53, 195, 237);
+    border: none;
+    text-transform: uppercase;
+  }
   div {
     max-width: 100%;
     height: 100%;
--- src/MapMarker.svelte
+++ src/MapMarker.svelte
@@ -11,8 +11,8 @@
   export let icon;
 
   const popup = new mapbox.Popup({ offset: 25 }).setText(label);
-
   const el = document.createElement("div");
+  
   el.className = "textMarker";
   el.style.display = "block";
   el.style.width = "50px";
@@ -21,12 +21,18 @@
   el.style.backgroundSize = "50px";
   el.style.backgroundRepeat = "no-repeat";
 
-  // el.addEventListener("click", function () {
-  //   window.alert(marker.properties.message);
-  // });
-
   const marker = new mapbox.Marker(el)
     .setLngLat([lon, lat])
     .setPopup(popup)
     .addTo(map);
 </script>
+
+<style>
+  .textMarker:after {
+    content: '';
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    background: blue;
+  }
+</style>
